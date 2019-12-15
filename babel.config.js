2c5191d9a884d546a24c585a7f2df93197660431
Cesar Puerta <capuertareiza@gmail.com>
2019-12-15 17:35:24 -0500

    Babel update for module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,14 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  env: {
-    development: {
-      plugins: [
-        [
-          'module-resolver',
-          {
-            alias: {
-              home: './app/modules/home',
-              store: './app/store',
-              styles: './app/styles'
-            }
-          }
-        ]
-      ]
-    }
-  }
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          home: './app/modules/home',
+          store: './app/store'
+        }
+      }
+    ]
+  ]
 };
